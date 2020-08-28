a0d0855e9510d94b50c9ddbd4f98bfe3f676bf1f
masatworks <masatworks118@gmail.com>
2020-08-28 11:48:12 +0900

    deleteの途中

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserCreateComponent } from './user/user-create/user-create.component';
 import { UserListComponent } from './user/user-list/user-list.component';
+import { UserShowComponent } from './user/user-show/user-show.component';
 import { UserEditComponent } from './user/user-edit/user-edit.component';
 
 const routes: Routes = [
@@ -11,7 +12,8 @@ const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'users/new', component: UserCreateComponent },
   { path: 'users', component: UserListComponent },
-  { path: 'users/:id/edit', component: UserEditComponent },
+  { path: 'users/:id', component: UserShowComponent },
+  { path: 'users/:id/edit', component: UserEditComponent }
 ];
 
 @NgModule({
